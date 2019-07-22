312a8800a940b7d2e1ee127b63917ceb95e1bc6c
fenghan970521 <1849072307@qq.com>
2019-07-22 16:51:37 +0800

    按钮修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,46 +2,41 @@
   <div class="hello">
     这是首页
     {{count}}
-    <p> <button @click="increment">+</button>
-                <button @click="decrement">-</button>
-                </p>
-   
+    <p>
+      <button @click="increment">+增加</button>
+      <button @click="decrement">-减少</button>
+    </p>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-     
+  name: "HelloWorld",
+  data() {
+    return {};
+  },
+  created() {
+    console.log(this.changePeople);
+  },
+  computed: {
+    changePeople() {
+      return this.$store.getters.changePeople;
+    },
+    count() {
+      return this.$store.state.count;
     }
   },
-  created(){
-    console.log(this.changePeople)
-  },
-  computed:{
-    changePeople(){
-      return this.$store.getters.changePeople
+  methods: {
+    increment() {
+      this.$store.commit("increment", 5);
     },
-    count(){
-      return this.$store.state.count
-    }
-  },
-  methods:{
-    increment(){
-      this.$store.commit('increment',5)
-    },
-    decrement(){
-      this.$store.commit('decrement',5)
-
-
+    decrement() {
+      this.$store.commit("decrement", 5);
     }
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
